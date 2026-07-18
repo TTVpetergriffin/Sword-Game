7304920c5d3c488a050ec82000130ad5e0663201
Pierce2025 <124064061+Pierce2025@users.noreply.github.com>
2026-07-18 16:33:45 -0400

    Update Gun.cs

--- Sword Game/Assets/Scripts/Weapons/Gun.cs
+++ Sword Game/Assets/Scripts/Weapons/Gun.cs
@@ -1,21 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
 public class Gun : MonoBehaviour
 {
     public float shootCooldown;
     public float projectileSpeed;
     public GameObject Bullet;
+    public bool canShoot;
     void Start()
     {
-        
+        canShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        if (Input.GetKeyDown(KeyCode.Mouse1) && canShoot)
         {
         Instantiate(Bullet, transform.position, transform.rotation);
+        canShoot = false;
+        StartCoroutine(ShootCooldown());
         }
+    }
+    IEnumerator ShootCooldown()
+    {
+        yield return new WaitForSeconds(1.0f);
+        canShoot = true;
     }
 }
