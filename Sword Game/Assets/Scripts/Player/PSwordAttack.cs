56dd8671c4a46e3b1e45b47c9225225f9509936c
TTVpetergriffin <112569065+TTVpetergriffin@users.noreply.github.com>
2026-07-18 15:10:25 -0400

    Sword Swing Cooldown Implemented
    Making huge major bigly huge progress

--- Sword Game/Assets/Scripts/Player/PSwordAttack.cs
+++ Sword Game/Assets/Scripts/Player/PSwordAttack.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class PSwordAttack : MonoBehaviour
 {
     [SerializeField] private GameObject SwordSwingBasic;
+    public bool CanSwing;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CanSwing = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && CanSwing)
         {
             SwordSwingBasic.SetActive(true);
+            CanSwing = false;
             StartCoroutine(SwordStop());
+
         }
     }
     IEnumerator SwordStop()
     {
         yield return new WaitForSeconds(0.01f);
         SwordSwingBasic.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        CanSwing = true;
     }
 }
