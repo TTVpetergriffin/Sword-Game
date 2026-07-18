8f857ad93e2895e537f162e0f937516fcf22cbe0
Pierce2025 <124064061+Pierce2025@users.noreply.github.com>
2026-07-18 17:37:41 -0400

    cooldown
    bangbaang

--- Sword Game/Assets/Scripts/Player/playerController.cs
+++ Sword Game/Assets/Scripts/Player/playerController.cs
@@ -127,7 +127,7 @@ void Update()
     public void UpdateMoney(int MoneyDollar)
     {
         money += MoneyDollar;
-        moneytext.text = "Money: " + money;
+        moneytext.text = ": " + money;
     }
 }
 
--- Sword Game/Assets/Scripts/Weapons/Gun.cs
+++ Sword Game/Assets/Scripts/Weapons/Gun.cs
@@ -1,30 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class Gun : MonoBehaviour
 {
     public float shootCooldown;
     public float projectileSpeed;
     public GameObject Bullet;
     public bool canShoot;
+    public Slider cooldownSlider;
+    public GameObject cooldownSliderObject;
+    public float cooldownTime = 1.0f;
+    public float currentCooldown = 0.0f;
+    public bool isCooldownActive = false;
     void Start()
     {
         canShoot = true;
+        cooldownSlider.value = 1.0f;
+        cooldownSliderObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isCooldownActive)
+        {
+            currentCooldown -= Time.deltaTime;
+            cooldownSlider.value = currentCooldown / cooldownTime; 
+        }
+        
         if (Input.GetKeyDown(KeyCode.Mouse1) && canShoot)
         {
         Instantiate(Bullet, transform.position, transform.rotation);
+        isCooldownActive = true;
         canShoot = false;
         StartCoroutine(ShootCooldown());
+        cooldownSliderObject.SetActive(true);
+        
         }
     }
     IEnumerator ShootCooldown()
     {
         yield return new WaitForSeconds(1.0f);
         canShoot = true;
+        cooldownSliderObject.SetActive(false);
+        isCooldownActive = false;
+        currentCooldown = 1.0f;
     }
 }
